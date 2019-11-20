c97193b48b549eadd3e6a84696f9c1a2189da7a6
Peter Generao <pgenerao516@gmail.com>
2019-11-19 23:22:49 -0800

    + Sanitize data in MyProjects Information Page so that it doesn't render raw html w/ tags + Implement submitting feedback change-requests for coaches + Implement approving request change-requests for coaches

--- app/controllers/appeditrequests_controller.rb
+++ app/controllers/appeditrequests_controller.rb
@@ -15,6 +15,25 @@ class AppeditrequestsController < ApplicationController
     @features_updated = @edit_request.features != @app.features
   end
 
+  def update
+    edit_request = AppEditRequest.find(params[:id])
+    app = App.find(edit_request.app_id)
+    unless params[:feedback_given]
+      app.description = edit_request.description
+      app.features = edit_request.features
+      app.save!
+      edit_request.destroy
+      redirect_to appeditrequests_path, alert: "Changes approved for #{app.name}"
+    else
+      new_request = edit_request.dup
+      new_request.feedback = params[:feedback]
+      new_request.status = :reviewed
+      edit_request.destroy
+      new_request.save
+      redirect_to appeditrequests_path, alert: "Feedback submitted for #{app.name}"
+    end
+  end
+
   def auth_user?
     User.find_by_id(session[:user_id])&.coach?
   end
